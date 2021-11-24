bfa3f3fabf6e1cd3b549845aabe2ffd3d24051ef
Mery <maria.touceda.suarez@gmail.com>
2021-11-24 18:05:05 -0500

    tests almost all adapted

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ import random
 import string
 from subprocess import getstatusoutput
 from shutil import rmtree
-from new import get_defaults
+from new import get_defaults  # what is happening here?
 
 PRG = './new_job.py'
-GOOD1 = './expected/inputs1.slurm'
-GOOD2 = './expected/inputs1.pbs'
-GOOD3 = './expected/inputs2.slurm'
-GOOD4 = './expected/inputs2.pbs'
+# GOOD1 = './expected/inputs1.slurm'  # only need it if I use other types of tests
+# GOOD2 = './expected/inputs1.pbs'
+# GOOD3 = './expected/inputs2.slurm'
+# GOOD4 = './expected/inputs2.pbs'
 
 # --------------------------------------------------
 def test_exists():
@@ -35,51 +35,63 @@ def test_usage():
         assert re.match("usage", out, re.IGNORECASE)
 
 # --------------------------------------------------
+
+
 def test_badsystem():
-    """usage"""
+    """test if code crashes weh bad system"""
 
     for flag in ['-s', '--system']:
         rv, out = getstatusoutput(f'{PRG} {flag} foo')
-        assert rv != 0
-        assert usage.lower().startswith('usage:')
+        assert rv != 0  # test that code breaks
+        assert out.lower().startswith('usage:') # test that error message is correct
 
 # --------------------------------------------------
-def test_filenotok():
-    """ bla """
-    pass
 
-# --------------------------------------------------
+
 def test_filenotok():
-    pass
+    """ test that code breaks when file is not given """
+
+    # given only the program with no positional job name argument
+    rv, out = getstatusoutput(f'{PRG}')
+    assert rv != 0  # test that code breaks
+    assert out.lower().startswith('usage:') # test that error message is correct
+
 
 # --------------------------------------------------
 def test_ok():
-    """ Runs ok """
+    """ Runs ok
+    This test will run things and tidy up after """
 
-    cwd = os.path.abspath(os.getcwd())
-    dirname = os.path.join(cwd, random_string())
-    abs_new = os.path.abspath(PRG)
+    cwd = os.path.abspath(os.getcwd())  # get current wd absolute path
+    dirname = os.path.join(cwd, random_string())  # create a random dirname
+    abs_new = os.path.abspath(PRG)  # get absolute path of program
 
-    if os.path.isdir(dirname):
+    if os.path.isdir(dirname):  # if dir exists remove -r
         rmtree(dirname)
 
-    os.makedirs(dirname)
+    os.makedirs(dirname)  # create the dir and cd
     os.chdir(dirname)
 
     try:
-        basename = random_string()
-        name = basename + '.py'
-        retval, out = getstatusoutput(f'{abs_new} -t {name}')
+        # check that file is created ok
+        basename = random_string()  # create random py script
+        name = basename + '.slurm'
+        retval, out = getstatusoutput(f'{abs_new} {name}')
         assert retval == 0
         assert os.path.isfile(name)
-        assert out == f'Done, see new script "{name}".'
+        assert out == f'Slurm job has been created!".'
 
-    finally:
+        # check that when -s pbs it is a pbs job
+        # NOTE: still not done this ^
+
+    finally:  # clean up
         if os.path.isdir(dirname):
             rmtree(dirname)
         os.chdir(cwd)
 
 # --------------------------------------------------
+
+
 def test_get_defaults():
     """ Test get_defaults() """
 
@@ -98,6 +110,7 @@ def random_string():
 
     k = random.randint(5, 10)
     return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
